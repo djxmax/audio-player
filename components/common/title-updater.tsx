6195e8ac1db4ec2b1a4e35367857f49e45f93154
Maxime Lucquin <mlucquin@agendize.com>
2026-02-10 10:38:42 +0100

    Ajout des info de lecture dans la media session api

--- components/common/title-updater.tsx
+++ components/common/title-updater.tsx
@@ -5,14 +5,34 @@ import { usePlayerStore } from "@/store/player-store";
 
 export default function TitleUpdater() {
   const currentTrack = usePlayerStore((state) => state.currentTrack);
+  const isPlaying = usePlayerStore((state) => state.isPlaying);
 
   useEffect(() => {
     if (currentTrack) {
       document.title = `${currentTrack.title} - WaveWolf`;
+
+      if ("mediaSession" in navigator) {
+        navigator.mediaSession.metadata = new MediaMetadata({
+          title: currentTrack.title,
+          artist: currentTrack.artist,
+          album: currentTrack.album,
+          artwork: currentTrack.coverUrl
+            ? [
+                {
+                  src: currentTrack.coverUrl,
+                  sizes: "256x256",
+                  type: "image/png",
+                },
+              ]
+            : [],
+        });
+
+        navigator.mediaSession.playbackState = isPlaying ? "playing" : "paused";
+      }
     } else {
       document.title = "WaveWolf";
     }
-  }, [currentTrack]);
+  }, [currentTrack, isPlaying]);
 
   return null;
 }
